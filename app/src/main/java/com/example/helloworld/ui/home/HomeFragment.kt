a05bfdce9f2477bd8948f41d4f2803409bb9c9a9
vakho84 <1zZ20plx3>
2023-12-13 16:37:58 +0400

    Used Glide for image load

--- app/src/main/java/com/example/helloworld/ui/home/HomeFragment.kt
+++ app/src/main/java/com/example/helloworld/ui/home/HomeFragment.kt
@@ -11,6 +11,7 @@ import android.view.inputmethod.EditorInfo.IME_ACTION_GO
 import android.view.inputmethod.EditorInfo.IME_FLAG_NO_EXTRACT_UI
 import android.widget.ImageView
 import androidx.fragment.app.Fragment
+import com.bumptech.glide.Glide
 import com.example.helloworld.databinding.FragmentHomeBinding
 import java.io.BufferedInputStream
 import java.net.URL
@@ -32,13 +33,14 @@ class HomeFragment : Fragment() {
         _binding = FragmentHomeBinding.inflate(inflater, container, false)
         val root: View = binding.root
 
-        var customUrl = "https://all-aforizmy.ru/wp-content/uploads/2023/05/ap.jpg"
-        binding.homeImageView.loadFromUrl(customUrl)
+        val customUrl = "https://all-aforizmy.ru/wp-content/uploads/2023/05/ap.jpg"
+//        binding.homeImageView.loadFromUrl(customUrl)
+        Glide.with(this).load(customUrl).into(binding.homeImageView)
 
 
         // edit text enter key listener
         val editTextInputUrl = binding.homeUrlInput
-        editTextInputUrl.setOnKeyListener(object : View.OnKeyListener {
+      /*  editTextInputUrl.setOnKeyListener(object : View.OnKeyListener {
             override fun onKey(v: View?, keyCode: Int, event: KeyEvent): Boolean {
                 // if the event is a key down event on the enter button
                 if (event.action == KeyEvent.ACTION_DOWN &&
@@ -56,11 +58,12 @@ class HomeFragment : Fragment() {
                 editTextInputUrl.requestFocus()
                 return false
             }
-        })
+        }) */
 
         val loadImage = binding.homeLoadImageButton
         loadImage.setOnClickListener(View.OnClickListener {
-            binding.homeImageView.loadFromUrl(customUrl)
+//            binding.homeImageView.loadFromUrl(editTextInputUrl.text.toString())
+            Glide.with(this).load(editTextInputUrl.text.toString()).into(binding.homeImageView)
         })
         return root
     }
