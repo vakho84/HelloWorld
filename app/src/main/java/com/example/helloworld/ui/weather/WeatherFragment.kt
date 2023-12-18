04b9ded31cd19ebec1816f498a5b6cfb6afba9d9
vakho84 <1zZ20plx3>
2023-12-18 15:35:03 +0400

    Repaired data save/load when fragment change

--- app/src/main/java/com/example/helloworld/ui/weather/WeatherFragment.kt
+++ app/src/main/java/com/example/helloworld/ui/weather/WeatherFragment.kt
@@ -8,7 +8,6 @@ import androidx.fragment.app.Fragment
 import com.bumptech.glide.Glide
 import com.example.helloworld.databinding.FragmentWeatherBinding
 import com.example.helloworld.retrofit.WeatherApi
-import com.example.helloworld.retrofit.WeatherResponseBody
 import com.google.gson.FieldNamingPolicy
 import com.google.gson.GsonBuilder
 import com.google.gson.JsonDeserializationContext
@@ -23,18 +22,40 @@ import java.lang.reflect.Type
 import java.time.LocalDateTime
 import java.time.format.DateTimeFormatter
 
-class WeatherFragment : Fragment() {
+private const val KEY_TEMP_C = "KEY_TEMP_C"
+private const val KEY_CITY = "KEY_CITY"
+private const val KEY_IMAGE_URL = "KEY_IMAGE_URL"
 
-   private val apiKey = "5924949e16a8492b9e8184723231212"
-  // private var city = "Tbilisi"
-    private lateinit var city: String
-    private lateinit var weather: WeatherResponseBody
-    private var isEmpty = false
+class WeatherFragment : Fragment() {
+    private val apiKey = "5924949e16a8492b9e8184723231212"
+
+    private lateinit var weatherViewModel: WeatherViewModel
+
+    private val weatherApi: WeatherApi
 
     private var _binding: FragmentWeatherBinding? = null
     // This property is only valid between onCreateView and
     // onDestroyView.
     private val binding get() = _binding!!
+
+    init {
+        val retrofit = Retrofit.Builder()
+            .baseUrl("https://api.weatherapi.com")
+            .addConverterFactory(GsonConverterFactory.create(
+                GsonBuilder()
+                    .setFieldNamingPolicy(FieldNamingPolicy.LOWER_CASE_WITH_UNDERSCORES)
+                    .registerTypeAdapter(LocalDateTime::class.java, object : JsonDeserializer<LocalDateTime> {
+                        override fun deserialize(json: JsonElement?, typeOfT: Type?, context: JsonDeserializationContext?): LocalDateTime {
+                            val s = json!!.asString
+                            val dateTimeFormatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm")
+                            return LocalDateTime.parse(s, dateTimeFormatter)
+                        }
+
+                    })
+                    .create())).build()
+
+        weatherApi = retrofit.create(WeatherApi::class.java)
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -42,53 +63,49 @@ class WeatherFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View {
 
-        _binding = FragmentWeatherBinding.inflate(inflater, container, false)
-        val root: View = binding.root
-        val button = binding.weatherLoadWeatherButton
-
-        val retrofit = Retrofit.Builder()
-            .baseUrl("https://api.weatherapi.com")
-            .addConverterFactory(GsonConverterFactory.create(
-                GsonBuilder()
-                .setFieldNamingPolicy(FieldNamingPolicy.LOWER_CASE_WITH_UNDERSCORES)
-                .registerTypeAdapter(LocalDateTime::class.java, object : JsonDeserializer<LocalDateTime> {
-                    override fun deserialize(json: JsonElement?, typeOfT: Type?, context: JsonDeserializationContext?): LocalDateTime {
-                        val s = json!!.asString
-                        val dateTimeFormatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm")
-                        return LocalDateTime.parse(s, dateTimeFormatter)
-                    }
-
-                })
-                .create())).build()
-
-        val weatherApi = retrofit.create(WeatherApi::class.java)
-        var  customUrl: String?
-
-
-        if ( isEmpty) {
-            binding.weatherTemperatureValue.text = weather.current.tempC.toString()
-            customUrl = "https:" + weather.current.condition.icon
-            Glide.with(requireContext()).load(customUrl).into(binding.weatherImage)
+        weatherViewModel = if (savedInstanceState == null) {
+            WeatherViewModel(null, "Tbilisi", null)
+        } else {
+            WeatherViewModel(
+                savedInstanceState.getFloat(KEY_TEMP_C),
+                savedInstanceState.getString(KEY_CITY)!!,
+                savedInstanceState.getString(KEY_IMAGE_URL)
+            )
         }
 
-        button.setOnClickListener {
+        _binding = FragmentWeatherBinding.inflate(inflater, container, false)
+
+        updateUi()
+
+        binding.weatherLoadWeatherButton.setOnClickListener {
+            val city =  binding.weatherCityInput.text.toString()
             CoroutineScope(Dispatchers.IO).launch {
-                if (binding.weatherCityInput.text.toString() == "")
-                {city = "Tbilisi"
-                    binding.weatherCityInput.setText(city)
-                }
-                else city = binding.weatherCityInput.text.toString()
-                 weather = weatherApi.getWeather(apiKey, city)
-                isEmpty = true
+                val weather = weatherApi.getWeather(apiKey, city)
                 activity?.runOnUiThread {
-                    binding.weatherTemperatureValue.text = weather.current.tempC.toString()
-                    customUrl = "https:" + weather.current.condition.icon
-                    Glide.with(requireContext()).load(customUrl).into(binding.weatherImage)
+                    weatherViewModel = WeatherViewModel(
+                        weather.current.tempC,
+                        city,
+                        "https:" + weather.current.condition.icon
+                    )
+                    updateUi()
                 }
             }
         }
 
-        return root
+        return binding.root
+    }
+
+    private fun updateUi() {
+        binding.weatherTemperatureValue.text = weatherViewModel.tempC?.toString() ?: ""
+        binding.weatherCityInput.setText(weatherViewModel.city)
+        Glide.with(requireContext()).load(weatherViewModel.imageUrl).into(binding.weatherImage)
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        weatherViewModel.tempC?.let { outState.putFloat(KEY_TEMP_C, it) }
+        outState.putString(KEY_CITY, weatherViewModel.city)
+        outState.putString(KEY_IMAGE_URL, weatherViewModel.imageUrl)
     }
 
     override fun onDestroyView() {
---
+++ app/src/main/java/com/example/helloworld/ui/weather/WeatherViewModel.kt
@@ -0,0 +1,7 @@
+package com.example.helloworld.ui.weather
+
+data class WeatherViewModel(
+    val tempC: Float?,
+    val city: String,
+    val imageUrl: String?
+)
